55e00362c04b19fe4d2d32a8dc6d85af6750e1f5
klawil <willyk95@gmail.com>
2018-09-14 19:45:04 -0700

    Update the test script to be more sophisticated

--- test.js
+++ test.js
@@ -1,16 +1,44 @@
-var noble = require('noble');
+const noble = require('noble');
+
+// Clear the console
+process.stdout.write('\x1Bc');
+
+// Used to determine which line we should use
+let index = 0;
+let stdout = process.stdout;
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning();
-  } else {
-    console.log('State: ' + state);
   }
 });
 
 noble.on('discover', function(p) {
-  console.log(`${p.advertisement.localName} (${p.address} [${p.addressType}]): ${p.rssi}`);
-  p.once('rssiUpdate', (rssi) => {
-    console.log(`${p.advertisement.localName} *(${p.address} [${p.addressType}]): ${rssi}`);
-  });
+  let localIndex = index;
+  index++;
+
+  let count = 1;
+
+  let lineStart = `${p.advertisement.localName} (${p.address} [${p.addressType}]): `;
+
+  stdout.cursorTo(localIndex);
+  stdout.write(`${lineStart} ${p.rssi} (${count})`);
+
+  function update() {
+    p.updateRssi((err, rssi) => {
+      setTimeout(update, 500);
+
+      stdout.cursorTo(localIndex);
+
+      if (err) {
+        stdout.write(`${lineStart} ${err}`);
+        return;
+      }
+
+      count++;
+      stdout.write(`${lineStart} ${rssi} (${count})`);
+    });
+  }
+
+  setTimeout(update, 500);
 });
