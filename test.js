750a9ad6b7b235cf35675736cdeaece89d17854a
klawil <klawil@a9.com>
2018-09-16 20:00:53 -0700

    Add distance formula to the output

--- test.js
+++ test.js
@@ -30,11 +30,30 @@ noble.on('discover', function(p) {
       ? 1
       : 2;
 
+  let dist = Math.round(distance(p.rssi, power) * 100) / 100;
+
   stdout.cursorTo(0, ids[p.address]);
-  stdout.write(`${lineStart} ${p.rssi} ${power} (${powerInt})`);
+  stdout.write(`${lineStart} ${dist} (${powerInt})`);
 });
 
 process.on('SIGINT', function() {
   stdout.cursorTo(0, index++);
   process.exit();
 });
+
+function distance(rssi, txPower) {
+  if (rssi === 0) {
+    return -1.0;
+  }
+
+  if (!txPower) {
+    txPower = -59;
+  }
+
+  const ratio = rssi * 1.0 / txPower;
+  if (ratio < 1.0) {
+    return Math.pow(ratio, 10);
+  }
+
+  return (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+}
