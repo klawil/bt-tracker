b5702ef7a8c7ec97b7fdf75457d0797a5c312ca8
klawil <klawil@a9.com>
2018-09-16 20:03:58 -0700

    Normalize the distance output

--- test.js
+++ test.js
@@ -32,8 +32,14 @@ noble.on('discover', function(p) {
 
   let dist = Math.round(distance(p.rssi, power) * 100) / 100;
 
+  if (dist > 10E4) {
+    dist = 'OVER  ';
+  } else {
+    dist = ('000000' + dist.toString()).slice(-6);
+  }
+
   stdout.cursorTo(0, ids[p.address]);
-  stdout.write(`${lineStart} ${dist} (${powerInt})`);
+  stdout.write(`${lineStart} ${dist} (${powerInt}, ${typeof power === 'undefined' ? 1 : 0})`);
 });
 
 process.on('SIGINT', function() {
