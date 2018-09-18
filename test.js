9e394ed76bdd847bd03cf1ee4e6674a93ab46737
klawil <willyk95@gmail.com>
2018-09-17 18:39:22 -0700

    Add better output

--- test.js
+++ test.js
@@ -5,8 +5,7 @@ const KalmanFilter = require('kalmanjs').default;
 process.stdout.write('\x1Bc');
 
 // Used to determine which line we should use
-let ids = {};
-let index = 0;
+let ids = [];
 let stdout = process.stdout;
 let longest = 0;
 let filters = {};
@@ -18,44 +17,39 @@ noble.on('stateChange', function(state) {
 });
 
 noble.on('discover', function(p) {
-  let lineStart = `${p.advertisement.localName} (${p.address} [${p.addressType}]):`;
+  let data = {
+    id: p.address,
+    lineStart: `${p.advertisement.localName} (${p.address} [${p.addressType}]):`
+  };
 
-  if (lineStart.length > longest) {
-    longest = lineStart.length;
+  if (data.lineStart.length > longest) {
+    longest = data.lineStart.length;
   }
 
-  lineStart = lineStart + ' '.repeat(longest - lineStart.length);
-
-  if (typeof ids[p.address] === 'undefined') {
-    ids[p.address] = index;
-    index++;
+  if (ids.filter((item) => item.id === data.id).length === 0) {
+    ids.push(data);
+  } else {
+    ids = ids.map((item) => item.id === data.id ? data : item);
   }
 
   let power = p.measuredPower || p.advertisement.txPower || p.advertisement.txPowerLevel;
+  data.powerType = typeof power === 'undefined'
+    ? 0
+    : 1;
 
-  let powerInt = p.measuredPower
+  data.powerInt = p.measuredPower
     ? 0
     : p.advertisement.txPower
       ? 1
       : 2;
 
-  let dist = Math.round(distance(p.address, p.rssi, power) * 100) / 100;
+  data.dist = Math.round(distance(p.address, p.rssi, power) * 100) / 100;
 
-  if (dist > 10E4) {
-    dist = 'OVER  ';
-  } else {
-    dist = dist.toString().split('.');
-    if (dist.length < 2) {
-      dist.push('00');
-    } else {
-      dist[1] = ('00' + dist[1]).slice(-2);
-    }
-
-    dist = ('000000' + dist.join('.')).slice(-6);
-  }
-
-  stdout.cursorTo(0, ids[p.address]);
-  stdout.write(`${lineStart} ${dist} (${powerInt}, ${typeof power === 'undefined' ? 1 : 0})`);
+  ids
+    .sort((a, b) => a.dist < b.dist 
+      ? -1
+      : 1)
+    .forEach(printData);
 });
 
 process.on('SIGINT', function() {
@@ -80,9 +74,7 @@ function distance(id, rssi, txPower) {
     distance = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
   }
 
-  distance = smoothDistance(id, distance);
-
-  return distance;
+  return smoothDistance(id, distance);
 }
 
 function smoothDistance(id, distance) {
@@ -91,4 +83,26 @@ function smoothDistance(id, distance) {
   }
 
   return filters[id].filter(distance);
+}
+
+function printData(data, line) {
+  stdout.cursorTo(0, line);
+
+  lineStart = data.lineStart + ' '.repeat(longest - data.lineStart.length);
+
+  let dist = data.dist;
+  if (dist > 10E4) {
+    dist = 'OVER  ';
+  } else {
+    dist = dist.toString().split('.');
+    if (dist.length < 2) {
+      dist.push('00');
+    } else {
+      dist[1] = ('00' + dist[1]).slice(-2);
+    }
+
+    dist = ('000000' + dist.join('.')).slice(-6);
+  }
+
+  stdout.write(`${lineStart} ${dist} (${data.powerInt}, ${data.powerType})`);
 }
